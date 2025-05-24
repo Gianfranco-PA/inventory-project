f8ddb8c4c71730ff2d3841c65a741e3370fbb7ca
Gianfranco <gfperez26@outlook.com>
2025-05-24 12:25:16 -0500

    ignore productId in json

--- stock-microservice/src/main/java/com/gianfranco/stock/dto/stock/StockDTO.java
+++ stock-microservice/src/main/java/com/gianfranco/stock/dto/stock/StockDTO.java
@@ -1,10 +1,12 @@
 package com.gianfranco.stock.dto.stock;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.time.LocalDateTime;
 
 public record StockDTO(
+        @JsonIgnore
         Long productId,
         Long quantity,
 
